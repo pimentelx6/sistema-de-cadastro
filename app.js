479417a6795c09914e1dcf5ff96ed40b96314929
pimentelx6 <pimentelx6@gmail.com>
2022-08-27 08:18:40 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("O paulo fez este commit sem você ver na aula")
+console.log("Enviando para o GitHub")
