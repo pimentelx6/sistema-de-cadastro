bb8b08f6cee547ce6973f2880283199fc4c0ef17
pimentelx6 <pimentelx6@gmail.com>
2022-08-28 06:27:49 -0300

    voltando para o arquivo alterado anteriormnente

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
-//mais um console log de teste:
-console.log("O paulo fez este commit sem você ver na aula")
-console.log("Enviando para o GitHub")
